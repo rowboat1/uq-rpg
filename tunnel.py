94496b36f5566640a33628a3f37c71ba6cbbdffc
Jordan-Mark <jordan.alexander.mark@gmail.com>
2023-08-26 15:42:16 +1000

    I just played with the colours really

--- tunnel.py
+++ tunnel.py
@@ -39,14 +39,15 @@ class Grid:
         self.grid_width = 19
         self.grid_height = 8
 
+        self.standard_colours = [pygame.Color(35, 15, 20), pygame.Color(25, 30, 25), pygame.Color(20, 35, 35)]
+
         self.rooms = random.randint(3,6) # number of rooms to generate
         self.room_origins = [] # list of room (x,y) basics
 
         #player origin must always be a tile 
         self.dict[(0,0)] = Tile(0, 0, "yellow", TILESIZE)
 
-        # the end of the level will also be in the bottom right corner
-        self.dict[(self.grid_width,self.grid_height)] = Tile(self.grid_width, self.grid_height, "red", TILESIZE)
+
 
         # get list of room centres
         for n in range(self.rooms):
@@ -56,7 +57,7 @@ class Grid:
 
         # place tiles at room centres
         for origin in self.room_origins:
-            self.dict[origin[0], origin[1]] = Tile(origin[0], origin[1], "grey", TILESIZE)
+            self.dict[origin[0], origin[1]] = Tile(origin[0], origin[1], pygame.Color(35, 35, 35), TILESIZE)
 
         # create paths to room origins
 
@@ -73,33 +74,36 @@ class Grid:
         #path to end
         self.path(self.room_origins[self.rooms-1], (self.grid_width,self.grid_height))
 
+        # the end of the level will also be in the bottom right corner
+        self.dict[(self.grid_width,self.grid_height)] = Tile(self.grid_width, self.grid_height, "red", TILESIZE)
+
 
     def path(self, start, finish):
 
         if start[0]<finish[0]:
             i=1
             while(start[0]+i!=finish[0]+1):
-                self.dict[start[0]+i, start[1]] = Tile(start[0]+i, start[1], "blue", TILESIZE)
+                self.dict[start[0]+i, start[1]] = Tile(start[0]+i, start[1], random.choice(self.standard_colours), TILESIZE)
                 i += 1
 
         if start[0]>finish[0]:
             i=1
             while(start[0]-i!=finish[0]-1):
-                self.dict[start[0]-i, start[1]] = Tile(start[0]-i, start[1], "blue", TILESIZE)
+                self.dict[start[0]-i, start[1]] = Tile(start[0]-i, start[1], random.choice(self.standard_colours), TILESIZE)
                 i += 1
 
 
         if start[1]<finish[1]:
             i=1
             while(start[1]+i!=finish[1]+1):
-                self.dict[finish[0], finish[1]-i] = Tile(finish[0], finish[1]-i, "white", TILESIZE)
+                self.dict[finish[0], finish[1]-i] = Tile(finish[0], finish[1]-i, random.choice(self.standard_colours), TILESIZE)
                 i += 1
 
         
         if start[1]>finish[1]:
             i=1
             while(start[1]-i!=finish[1]-1):
-                self.dict[finish[0], finish[1]+i] = Tile(finish[0], finish[1]+i, "white", TILESIZE)
+                self.dict[finish[0], finish[1]+i] = Tile(finish[0], finish[1]+i, random.choice(self.standard_colours), TILESIZE)
                 i += 1
         
 
